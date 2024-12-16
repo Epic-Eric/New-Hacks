0b3afc893b0e99b7ac9251d816404234c64f32a7
Paul Dong <paulkdong@gmail.com>
2024-12-16 11:32:09 -0500

    lobby bugging

--- app.py
+++ app.py
@@ -240,7 +240,7 @@ async def disconnect(sid):
 @sio.event
 async def webcam_data(sid, data):
     # Process the webcam data (base64 image)
-    print(data)
+    print(f'data received: {data}')
     lobby_code = data['lobbyCode']
     lobby = lobbies.get(lobby_code)
 
